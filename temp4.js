23a8eb0446d295e787d622ce0b9120f9a71d50d0
mirul786 <mirulkhan786@gmail.com>
2022-12-26 20:25:44 +0530

    tempcooment

---
+++ temp4.js
@@ -0,0 +1,23 @@
+for (let i = 0; i<=5; i++){
+  
+if(i == 4){
+    
+continue;
+  
+}
+  
+console.log(i)
+
+}
+
+for (let i = 0; i<=5; i++){
+  
+console.log(i)
+  
+if(i == 4){
+    
+continue;
+  
+}
+
+}
